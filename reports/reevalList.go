9c8622a79b06ad37e3afad3aaefb9e6559c2115f
LadySqrrl <72673087+LadySqrrl@users.noreply.github.com>
2022-02-05 11:55:45 -0600

    add to reports

--- reports/reevalList.go
+++ reports/reevalList.go
@@ -56,3 +56,46 @@ func ReevalList() []Reeval {
 
 	return reevals
 }
+
+// ReevalsByMonth creates a slice of Reevals due in a given month
+func ReevalsByMonth() []Reeval {
+	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3307)/cases?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	monthStart := "2021-02-01"
+	monthEnd := "2021-03-01"
+
+	rows, err := db.Query(
+		`SELECT CONCAT(s.student_first_name, ' ', s.student_last_name), p.provider_name, s.reeval_due,
+		CASE WHEN s.annual_due < s.reeval_due THEN s.annual_due ELSE s.reeval_due END AS "Meeting" 
+		FROM students s
+		INNER JOIN providers p 
+		ON s.providerID = p.providerID
+		WHERE s.reeval_due BETWEEN '` + monthStart + `' AND '` + monthEnd + `' ORDER BY s.reeval_due;`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var reevals []Reeval
+	for rows.Next() {
+		var r Reeval
+
+		err := rows.Scan(&r.name, &r.providerID, &r.reevalDate, &r.meetingDate)
+		if err != nil {
+			log.Fatal(err)
+		}
+		reevals = append(reevals, r)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return reevals
+}
